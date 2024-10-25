1da726866b135dd97124fc6481a086c405c65993
TuanDQ3993 <quangtuan3903@gmail.com>
2024-10-25 14:48:12 +0700

    update filter search

--- ProjectPRN221_LIBManagement/Pages/Admin/BooksManage.cshtml.cs
+++ ProjectPRN221_LIBManagement/Pages/Admin/BooksManage.cshtml.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ProjectPRN221_LIBManagement.Models;
@@ -14,12 +14,53 @@ namespace ProjectPRN221_LIBManagement.Pages.Admin
             _context = context;
         }
         public IList<Book> Books { get; set; }
+        public IList<Author> authors = new List<Author>();
+        public IList<Publisher> publishers = new List<Publisher>();
+        public IList<Category> categories = new List<Category>();
 
-        public async Task OngetAsync()
+        public int? pId { get; set; }
+        public int? aId {get; set;}
+
+        public int? cid { get; set; }
+        
+        public string search {  get; set; }
+        public async Task OnGetAsync(string searchString, int? publisherId, int? authorId, int? categoryId)
         {
-            Books = await _context.Books.Include(x => x.Author)
-                .Include(x => x.Category)
-                .Include(x => x.Publisher).ToListAsync();
+
+            IQueryable<Book> booksQuery = _context.Books.Include(x => x.Author)
+                                                        .Include(x => x.Category)
+                                                        .Include(x => x.Publisher);
+            authors = _context.Authors.ToList();
+            publishers = _context.Publishers.ToList();
+            categories = _context.Categories.ToList();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.ToUpper();
+                booksQuery = booksQuery.Where(b => b.Title.ToUpper().Contains(searchString) ||
+                                                   b.Isbn.ToUpper().Contains(searchString));
+                search = searchString;
+            }
+            if (authorId> 0)
+            {
+                booksQuery = booksQuery.Where(b => b.AuthorId == authorId);
+              aId = authorId;
+            }
+            if (categoryId > 0) {
+                booksQuery = booksQuery.Where(b => b.CategoryId == categoryId);
+                cid= categoryId;
+            }
+            if (publisherId > 0) {
+                booksQuery = booksQuery.Where(b => b.PublisherId == publisherId);
+                pId = publisherId;
+            }
+
+
+
+
+            Books = await booksQuery.ToListAsync();
+           
+
         }
     }
 }
--- ProjectPRN221_LIBManagement/obj/Debug/net6.0/ProjectPRN221_LIBManagement.AssemblyInfo.cs
+++ ProjectPRN221_LIBManagement/obj/Debug/net6.0/ProjectPRN221_LIBManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPRN221_LIBManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e1161d44f2d225b6a599fe1c4dcf29b0a11e478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2acffc52677578847cf348d8390447139c0dd8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPRN221_LIBManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPRN221_LIBManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
