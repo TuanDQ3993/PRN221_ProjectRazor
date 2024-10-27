83ed97b74d5c82db76ed9f4fce57007880a6365e
TuanDQ3993 <quangtuan3903@gmail.com>
2024-10-27 16:28:39 +0700

    template user

---
+++ ProjectPRN221_LIBManagement/Pages/Home/Shop.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectPRN221_LIBManagement.Pages.Home
+{
+    public class ShopModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
